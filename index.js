fbbd108a9618dec52e13c7dcf3cd789cf442cb56
i am dk <iamdk353@gmail.com>
2024-05-19 18:39:29 +0530

    intial setup and basic routing

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("server started\n  http://localhost:" + port);
+});
